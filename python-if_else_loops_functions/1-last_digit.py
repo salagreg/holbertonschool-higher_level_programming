1cae162ab897bf94d43338c627ce1ffcf614fdb3
salagreg <greg.sala@icloud.com>
2025-09-03 22:26:44 +0200

    Ajout fichier task 1 dans repo

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,3 +8,5 @@ elif end == 0:
     print(f"Last digit of {number} is {end} and is 0")
 elif end < 6:
     print(f"Last digit of {number} is {end} and is less than 6 and not 0")
+else:
+    print("NULL")
