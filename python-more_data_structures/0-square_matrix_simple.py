4c301de0f0fcd734d87ce7153807b2440753a9f6
salagreg <greg.sala@icloud.com>
2025-09-09 18:43:15 +0200

    Calculer valeur au carré de tous les entiers

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_row = []
+        for j in i:
+            new_row.append(j ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
