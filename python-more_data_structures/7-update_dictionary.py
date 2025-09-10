54da6f714ba3810523381a98814b169cb2ea919c
salagreg <greg.sala@icloud.com>
2025-09-10 15:45:48 +0200

    Remplacer ou ajouter une clé dans un dictionnaire

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # Remplace ou ajoute une clé/valeur dans un dictionnaire
+    a_dictionary[key] = value
+    return a_dictionary
