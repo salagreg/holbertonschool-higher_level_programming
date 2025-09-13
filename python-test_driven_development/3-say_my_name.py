5a0efc4a1e3715142b8cd27571597ae1d3fb5d84
salagreg <greg.sala@icloud.com>
2025-09-13 15:20:57 +0200

    Ajout fichiers Task 2 dans repository

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+Ce module fournit une fonction permettant d'afficher un nom complet
+au format : "My name is <first name> <last name>".
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    Affiche le nom complet sous la forme : "My name is <first name> <last name>".
+
+    Paramètres :
+        first_name (str) : Le prénom de la personne. Doit être une chaîne de caractères.
+        last_name (str, optionnel) : Le nom de famille de la personne. Par défaut : chaîne vide.
+
+    Exceptions :
+        TypeError : Si first_name ou last_name ne sont pas des chaînes de caractères.
+
+    Affiche :
+        Le nom complet formaté.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
