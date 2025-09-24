44e1634f7dcc461acee5e2ab5679790a16a1f8f9
salagreg <greg.sala@icloud.com>
2025-09-24 18:13:49 +0200

    Create Abstract Animal Class and its Subclasses

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
+"""
+This module defines an abstract base class `Animal` using Python's abc module.
+"""
+
+
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
-    
+  """Abstract base class representing an animal."""
+
+
   @abstractmethod
   def sound(self):
+      """Abstract method that must be implemented by all subclasses."""
       pass
   
 class Dog(Animal):
+    """Dog class that implements the sound method."""
+
+
     def sound(self):
       return "Bark"
     
 
 class Cat(Animal):
+    """Cat class that implements the sound method."""
+
+    
     def sound(self):
       return "Meow"
