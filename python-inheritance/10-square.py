aeaf5b7e1f279c52996a73c376938d3426ac7a0f
salagreg <greg.sala@icloud.com>
2025-09-26 13:42:51 +0200

    Update 10-square

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,38 +5,7 @@ a rectangle class that inherits from it.
 """
 
 
-class BaseGeometry:
-    """
-    BaseGeometry is a base class for other geometry classes.
-    """
-    def area(self):
-        """
-        Compute the area of the geometry shape.
-
-        Raises:
-            Exception: Always raises an exception to indicate that the
-            method is not implemented in the base class.
-
-        This method is meant to be overridden by subclasses.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate that 'value' is a positive integer.
-
-        Args:
-        name (str): The name of the parameter to validate.
-        value (int): The value to validate.
-
-        Raises:
-        TypeError: If 'value' is not an integer.
-        ValueError: If 'value' is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
