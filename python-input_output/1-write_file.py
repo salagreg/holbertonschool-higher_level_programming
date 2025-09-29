5da246dba1fc236338ede11b8180021fb0d73cef
salagreg <greg.sala@icloud.com>
2025-09-29 16:02:30 +0200

    Write a string to a text file and returns the number of characters written

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module that write a string to a text file (UTF8)
+    and returns the number of characters written """
+
+
+def write_file(filename="", text=""):
+    """ Write a string to a text file (UTF8)
+        and returns the number of characters written """
+    with open(filename, 'w', encoding="UTF-8") as f:
+        return f.write(text)
