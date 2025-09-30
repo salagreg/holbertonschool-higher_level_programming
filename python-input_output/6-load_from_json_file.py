8eb21cdb5e50c461b73ae0964ac0b01ada260346
salagreg <greg.sala@icloud.com>
2025-09-30 12:11:31 +0200

    Create an object from a json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module that creates an object from a json file """
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ Create an object from a json file """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
