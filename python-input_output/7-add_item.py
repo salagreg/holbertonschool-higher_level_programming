887888bef9550750f5147af38ae5ad61902bb2b1
salagreg <greg.sala@icloud.com>
2025-10-01 12:40:19 +0200

    Adds all arguments to a Python list and then saves them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list and then saves them to a file"""
+""" Module that adds all arguments to a Python list
+    and then saves them to a file """
 
 
 import sys
