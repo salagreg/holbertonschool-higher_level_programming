753bf896aef79fba46f7ad9b4824976a90e712a4
salagreg <greg.sala@icloud.com>
2025-10-08 22:54:25 +0200

    MAJ fichier task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,15 +2,13 @@
 """Module qui crée une API simple avec Flask"""
 
 
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    """Affiche un message d'accueuil"""
+    """Affiche un message d'accueil"""
     return "Welcome to the Flask API!"
 
 
@@ -37,7 +35,7 @@ def new_users(username):
     if username in users:
         return jsonify(users[username])
     else:
-        return "404 Not Found"
+        return jsonify({"error": "User not found"}), 404
 
 
 @app.route("/add_user", methods=["POST"])
@@ -45,13 +43,18 @@ def add_user():
     """Reçoit des données JSON, ajoute un nouvel utilisateur
         dans un dictionnaire, retourne un message de confirmation"""
     data = request.get_json()
+
+    if "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+
     username = data["username"]
     users[username] = {
+        "username": username,
         "name": data["name"],
         "age": data["age"],
         "city": data["city"]
     }
-    return jsonify({"message": "User added", "user": users[username]})
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
