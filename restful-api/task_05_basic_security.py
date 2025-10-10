ed3e3894a719c0e9bd69be2c3a2eb0c9d4552c25
salagreg <greg.sala@icloud.com>
2025-10-10 17:51:51 +0200

    MAJ task 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -8,12 +8,13 @@ from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt
 
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-
+app.config["JWT_SECRET_KEY"] = "c2Z_q54EbJqX3YX_gnFEHBq23i9-UPCkL2d0dibFvPM"
+jwt = JWTManager(app)
 
 users = {
     "user1": {
@@ -42,16 +43,42 @@ def verify_password(username, password):
         return False
 
 
+@auth.error_handler
+def unauthorized_error():
+    """Gère le cas d’accès non autorisé pour l’authentification basique."""
+    return jsonify({"error": "Unauthorized access"}), 401
+
+
 @app.route('/basic-protected', methods=['GET'])
 @auth.login_required
 def basic_protected():
     """Route protégée par authentification basique."""
 
-    return jsonify({"message": "Basic Auth: Access Granted"}), 200
+    return "Basic Auth: Access Granted"
 
 
-app.config["JWT_SECRET_KEY"] = "c2Z_q54EbJqX3YX_gnFEHBq23i9-UPCkL2d0dibFvPM"
-jwt = JWTManager(app)
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    """Gère le cas où aucun jeton JWT n’est fourni ou qu’il est invalide"""
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    """Gère le cas où le jeton JWT fourni est mal formé ou invalide"""
+    return jsonify({"error": "Invalid token"}), 401
+
+
+@jwt.expired_token_loader
+def handle_expired_token_error(jwt_header, jwt_payload):
+    """Gère le cas où le jeton JWT a expiré"""
+    return jsonify({"error": "Token has expired"}), 401
+
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(jwt_header, jwt_payload):
+    """Gère le cas où le jeton JWT a été révoqué"""
+    return jsonify({"error": "Token has been revoked"}), 401
 
 
 @app.route('/login', methods=["POST"])
@@ -63,17 +90,14 @@ def login_jwt():
     password = data.get("password")
 
     if username not in users:
-        return jsonify({"Erreur": "Utilisateur introuvable"}), 401
+        return jsonify({"error": "Utilisateur introuvable"}), 401
 
     if not check_password_hash(users[username]["password"], password):
-        return jsonify({"Erreur": "Mot de passe invalide"}), 401
+        return jsonify({"error": "Mot de passe invalide"}), 401
 
     access_token = create_access_token(
-        identity={
-            "username": username,
-            "role": users[username]["role"]
-        }
-    )
+        identity=username,
+        additional_claims={"role": users[username]["role"]})
 
     return jsonify({"access_token": access_token}), 200
 
@@ -82,22 +106,17 @@ def login_jwt():
 @jwt_required()
 def protected_jwt():
     """Route protégée par un jeton JWT."""
-
-    identity = get_jwt_identity()
-    username = identity["username"]
-    return jsonify({"msg": f"JWT Auth: Access Granted for {username}"}), 200
+    return "JWT Auth: Access Granted"
 
 
 @app.route('/admin-only', methods=["GET"])
 @jwt_required()
 def admin():
-    """Route réservée uniquement aux administrateurs."""
+    claims = get_jwt()
+    if claims.get("role") != "admin":
+        return jsonify({"error": "Admin access required"}), 401
 
-    identity = get_jwt_identity()
-    if identity["role"] != "admin":
-        return jsonify({"error": "Admin access required"}), 403
-    else:
-        return jsonify({"message": "Admin Access: Granted"}), 200
+    return "Admin Access: Granted"
 
 
 if __name__ == "__main__":
