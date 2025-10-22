8cfa21c781e0f52562c6163675d18f546928a1db
salagreg <greg.sala@icloud.com>
2025-10-22 18:14:42 +0200

    Mis à jour fichier

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- Crée un utilisateur dans le serveur de base de données MySQL.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
