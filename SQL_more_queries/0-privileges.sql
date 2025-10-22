b79791f9fe1a6a0807e1c38d3d1b8da06c8dc107
salagreg <greg.sala@icloud.com>
2025-10-22 18:06:49 +0200

    Mis à jour fichier

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Répertorie les privilèges accordés à chaque utilisateur dans la base de données.
-SHOW GRANTS FOR "user_0d_1"@"localhost";
-SHOW GRANTS FOR "user_0d_2"@"localhost";
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
