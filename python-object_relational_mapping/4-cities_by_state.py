e5dac6149611df4f5edb8815885f7b1f4a5c0b20
salagreg <greg.sala@icloud.com>
2025-10-24 18:46:09 +0200

    Lister toutes les villes de la base de données

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Module qui affiche toutes les villes de la base de données"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cur = db.cursor()
+    cur.execute(
+                "SELECT cities.id, cities.name, states.name "
+                "FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC"
+            )
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
