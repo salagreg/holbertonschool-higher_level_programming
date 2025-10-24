62dfc2aeed419e1f70d3cac2b2c59d65e974f742
salagreg <greg.sala@icloud.com>
2025-10-24 23:12:34 +0200

    Mis à jour task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Module qui affiche toutes les villes d'un état donné"""
 
 
@@ -20,19 +20,19 @@ if __name__ == "__main__":
         db=db_name
     )
 
-    query = (
-        "SELECT cities.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id ASC"
-      )
-
     cur = db.cursor()
-    cur.execute(query, (state_name,))
-    rows = cur.fetchall()
-    for row in rows:
-        print(", ".join([row[0] for row in rows]))
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
 
-        cur.close()
-        db.close()
+    cur.execute(query, (state_name,))
+
+    cities = [row[0] for row in cur.fetchall()]
+    print(", ".join(cities))
+
+    cur.close()
+    db.close()
