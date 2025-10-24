3f6a4c0458e2f131e105d61975f396f57d2d61e9
salagreg <greg.sala@icloud.com>
2025-10-24 13:24:37 +0200

    Liste tous les etats d'une base de données

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
 
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
