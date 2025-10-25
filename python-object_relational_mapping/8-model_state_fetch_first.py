7927cea0a2b0027bceec4dbc341465a53a9d4360
salagreg <greg.sala@icloud.com>
2025-10-25 12:02:38 +0200

    Récupèrer et afficher le premier etat

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module qui récupère et affiche le premier état
+        de la base de données hbtn_0e_6_usa"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{database}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    for first_state in [first_state]:
+        print(f"{first_state.id}: {first_state.name}")
+
+    session.close()
