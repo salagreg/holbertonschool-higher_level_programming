195a3d3190350bb5cce4c643d9254b1f7c4bc8d6
salagreg <greg.sala@icloud.com>
2025-11-06 17:35:40 +0100

    Afficher un message à chaque nombre émis

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const number = parseInt(arg);
+
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < arg; i++) {
+    console.log("C is fun");
+  }
+}
