33a695b1955ef83443fa3b8eaea324e0f577ae75
salagreg <greg.sala@icloud.com>
2025-11-12 10:30:09 +0100

    Mise à jour du header

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const updateHeader = document.querySelector('#update_header');
+updateHeader.addEventListener('click', function () {
+  const header = document.querySelector('header');
+  header.textContent = 'New Header!!!';
+});
